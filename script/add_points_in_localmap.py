2c43a96a8c366d04c493ae200373d959a1fe4b7d
Donghee Han <hdh7485@gmail.com>
2019-07-02 16:35:36 +0900

    Add mission number subscriber and geofence_left_lane.csv file

--- script/add_points_in_localmap.py
+++ script/add_points_in_localmap.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import Pose
 from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud2 
+from lcm_to_ros.msg import hyundai_mission
 
 import tf2_ros
 import tf2_py as tf2
@@ -21,13 +22,15 @@ class CSVRepublisher:
         rospy.init_node("csv_to_map", anonymous=True)
 
         rospack = rospkg.RosPack()
-        left_geofence_path = rospack.get_path("csv_to_map") + "/csv_map/geofence_first_lane.csv"
+        #left_geofence_path = rospack.get_path("csv_to_map") + "/csv_map/geofence_first_lane.csv"
+        left_geofence_path = rospack.get_path("csv_to_map") + "/csv_map/geofence_left_lane.csv"
         right_geofence_path = rospack.get_path("csv_to_map") + "/csv_map/geofence_right_shift.csv"
 
         self.left_wall_csv_data = self.read_csv(left_geofence_path)
         self.right_wall_csv_data = self.read_csv(right_geofence_path)
         self.entire_csv_data = self.left_wall_csv_data
         [self.entire_csv_data.append(element) for element in self.right_wall_csv_data]
+        #self.entire_csv_data = self.right_wall_csv_data
 
         self.odom_header = Header()
         self.odom_header.frame_id = "/odom"
@@ -35,9 +38,12 @@ class CSVRepublisher:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
+        self.mission_number = 0
+
         self.map_pub = rospy.Publisher("/csv_override_map", OccupancyGrid, queue_size=1)
         self.pose_array_pub = rospy.Publisher("/csv_pose_array", PoseArray, queue_size=1)
         rospy.Subscriber("/local_map", OccupancyGrid, self.map_callback)
+        rospy.Subscriber("/lcm_to_ros/LCM2ROS_mission", hyundai_mission, self.mission_num_callback)
 
     def read_csv(self, file_path):
         csv_data = []
@@ -48,31 +54,36 @@ class CSVRepublisher:
                 float_line = [float(i) for i in line]
                 csv_data.append(float_line)
         return csv_data
+
+    def mission_num_callback(self, hyundai_data):
+        self.mission_number = hyundai_data.mission_number
+        
     
     def map_callback(self, map_data):
-        time1 = time.time()
-        self.tf_csv_data = self.csv_transform(self.entire_csv_data)
-        time2 = time.time()
+        if self.mission_number == 6:
+            time1 = time.time()
+            self.tf_csv_data = self.csv_transform(self.entire_csv_data)
+            time2 = time.time()
 
-        out_map = map_data
-        out_map.header.frame_id = "/base_footprint"
-        out_map.header.stamp = rospy.Time.now()
-        out_data = list(out_map.data)
+            out_map = map_data
+            out_map.header.frame_id = "/base_footprint"
+            out_map.header.stamp = rospy.Time.now()
+            out_data = list(out_map.data)
 
-        pose_array = PoseArray()
-        time3 = time.time()
-        for point in self.tf_csv_data:    
-            index = int((point[1] - map_data.info.origin.position.y)/map_data.info.resolution) * int(map_data.info.width) + int((point[0] - map_data.info.origin.position.x)/map_data.info.resolution)
-            if index < len(out_data) and index >= 0:
+            pose_array = PoseArray()
+            time3 = time.time()
+            for point in self.tf_csv_data:    
+                index = int((point[1] - map_data.info.origin.position.y)/map_data.info.resolution) * int(map_data.info.width) + int((point[0] - map_data.info.origin.position.x)/map_data.info.resolution)
                 #rospy.loginfo("%d", index)
-                out_data[index] = 100
+                if index < len(out_data) and index >= 0:
+                    out_data[index] = 100
 
-        out_map.data = tuple(out_data)
-        time4 = time.time()
+            out_map.data = tuple(out_data)
+            time4 = time.time()
 
-        self.map_pub.publish(out_map)
-        time5 = time.time()
-        rospy.loginfo("%f %f %f %f", time2 - time1, time3 - time2, time4 - time3, time5 - time4)
+            self.map_pub.publish(out_map)
+            time5 = time.time()
+            #rospy.loginfo("%f %f %f %f", time2 - time1, time3 - time2, time4 - time3, time5 - time4)
     
     def csv_transform(self, csv_data):
         #self.tf_listener.waitForTransform("/base_footprint", "/odom", rospy.Time(0), rospy.Duration(4.0))
@@ -81,15 +92,10 @@ class CSVRepublisher:
                                            rospy.Duration(4.0))
         t_kdl = transform_to_kdl(trans) 
         r = [t_kdl * PyKDL.Vector(p[1], p[0], 0.0) for p in csv_data]
-        
         return r
 
     def run(self):
         rospy.spin()
-        #r = rospy.Rate(100)
-        #while not rospy.is_shutdown():
-        #    rospy.spin()
-        #    r.sleep()
 
 if __name__ == "__main__":
     repub = CSVRepublisher()
